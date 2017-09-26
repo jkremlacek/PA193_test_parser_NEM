2c5e915f3323fc748b3dc323b65853c2fcc1801e
jkremlacek <jkremlacek@email.cz>
2017-09-26 14:43:38 +0200

    Changed tests, added catch.hpp
    because why make it simple when it can be complicated

--- src/test/testing.cpp
+++ src/test/testing.cpp
@@ -1,1 +1,9 @@
 #include "../../stdafx.h"
+
+// Tell CATCH to define its main function here
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("Factorial testing", "[factorial]") {
+    CHECK(1 == 1);
+}
