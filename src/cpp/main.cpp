b854b6921780a0b399680a935c3318e3c9aba757
jkremlacek <jkremlacek@email.cz>
2017-09-26 15:16:53 +0200

    Base Impl

---
+++ src/cpp/main.cpp
@@ -0,0 +1,17 @@
+#include "../../stdafx.h"
+
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World" << endl;
+
+	int i;
+
+	cin >> i;
+
+	return 0;
+}
+
