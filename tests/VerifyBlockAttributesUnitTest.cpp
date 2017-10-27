98f62af0934ae14b78202c35f25133e6da732d0f
Richard Kalinec <rkalinec@gmail.com>
2017-10-27 23:26:17 +0200

    Retarget solution to the newest Windows SDK version and add VerifyBlockAttributesUnitTest.cpp torso
    Starting to implement tests for block validation, retargeting was
    necessary to make the solution work with the newest Windws SDK.

---
+++ tests/VerifyBlockAttributesUnitTest.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "..\src\h\model\Block.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace tests
+{
+
+}
